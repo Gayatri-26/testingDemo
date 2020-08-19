2dcff6dd72d906ea0a88563d79f869465b1903da
Mac <mac@Macs-MacBook-Pro.local>
2020-08-19 11:08:42 +0530

    Hello Everyone

---
+++ Git Testing Demo/Git Testing Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Testing Demo
+//
+//  Created by Mac on 19/08/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello Everyone")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
